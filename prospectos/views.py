da3f2f54ffdeeb398bf34577709713d5be60d1ab
Ruben Cornejo <sistemas@usa.edu.mx>
2015-03-26 13:15:33 -0600

    Formulario prospecto BUAP

--- prospectos/views.py
+++ prospectos/views.py
@@ -2,11 +2,13 @@ from django.contrib.auth.models import User
 from django.shortcuts import render_to_response, render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from prospectos.models import ProspectoForm, Prospecto
+from prospectos.models import ProspectoForm, Prospecto, ProspectoBuapForm
 from eventos.models import Evento
 from django.db.models import Count
 import pandas as pd
+from django.contrib import messages
 import numpy as np
+
 
 # Create your views here.
 @login_required
@@ -15,6 +17,7 @@ def prospecto_nuevo(request):
     If users are authenticated, direct them to the main page. Otherwise, take
     them to the login page.
     """
+    messages.set_level(request, messages.SUCCESS)
 # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -25,14 +28,46 @@ def prospecto_nuevo(request):
             post = form.save(commit=False)
             post.autor = request.user
             post.save()
+            messages.success(request, 'El Prospecto fue guardado correctamente.')
             # redirect to a new URL:
             return HttpResponseRedirect('/prospectos/nuevo/')
+        messages.warning(request, 'Verifica los datos faltantes o con error.')
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ProspectoForm()
 
     return render(request, 'prospectos/nuevo.html', {'form': form})
+
+@login_required
+def prospecto_buap(request):
+    """
+    If users are authenticated, direct them to the main page. Otherwise, take
+    them to the login page.
+    """
+# if this is a POST request we need to process the form data
+    messages.set_level(request, messages.SUCCESS)
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProspectoBuapForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            post = form.save(commit=False)
+            evento, _ = Evento.objects.get_or_create(evento=162)
+            post.evento = evento
+            post.autor = request.user
+            post.save()
+            messages.success(request, 'El Prospecto BUAP fue guardado correctamente.')
+            # redirect to a new URL:
+            return HttpResponseRedirect('/prospectos/nuevobuap/')
+        messages.warning(request, 'Verifica los datos faltantes o con error.')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ProspectoBuapForm()
+
+    return render(request, 'prospectos/buap.html', {'form': form})
 
 @login_required
 def captura(request):
